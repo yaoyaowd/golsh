7d1835a427f4f852cdd1a8b0d6d3f3752eafdc8e
dwangw <dwang@contextlogic.com>
2017-03-14 14:02:24 -0700

    add lsh forest

---
+++ forest_lsh.go
@@ -0,0 +1,105 @@
+package golsh
+
+type Node struct {
+	Level int
+	Key int
+	Ind []int
+	Children map[int]*Node
+	Father *Node
+}
+
+func (n *Node) insert(level, id int, key *HashKey) {
+	n.Ind = append(n.Ind, id)
+	if level != len(*key) {
+		var child *Node
+		if nodeChild, ok := n.Children[(*key)[level]]; !ok {
+			child = &Node{
+				level + 1,
+				(*key)[level],
+				make([]int,0),
+				make(map[int]*Node),
+				n,
+			}
+			n.Children[(*key)[level]] = child
+		} else {
+			child = nodeChild
+		}
+		child.insert(level + 1, id, key)
+	}
+}
+
+func (n *Node) lookup(hvs *HashKey) *Node {
+	c := n
+	for l := 0; l < len(*hvs); l++ {
+		if child, ok := c.Children[(*hvs)[l]]; ok {
+			c = child
+		} else {
+			break
+		}
+	}
+	return c
+}
+
+type ForestLSH struct {
+	*Params
+	Roots []*Node
+}
+
+func (lsh *ForestLSH) Query(p *Point, n int, out chan int) {
+	go func() {
+		hvs := lsh.Hash(p)
+		candidates := []int{}
+		nodes := []*Node{}
+		seens := map[int]bool{}
+		for l := lsh.KeySize; l >= 0; l-- {
+			for i, root := range lsh.Roots {
+				nodes = append(nodes, root.lookup(&hvs[i]))
+			}
+
+			for _, node := range nodes {
+				if node == nil || node.Level < l {
+					continue
+				}
+				for _, candidate := range node.Ind {
+					if _, seen := seens[candidate]; !seen {
+						seens[candidate] = true
+						candidates = append(candidates, candidate)
+					}
+				}
+				node = node.Father
+			}
+
+			if len(candidates) >= n {
+				break
+			}
+		}
+		for _, q := range candidates {
+			out <- q
+		}
+		out <- -1
+	}()
+}
+
+func NewForestLSH(params *Params) *ForestLSH {
+	roots := make([]*Node, params.TableSize)
+	for i, _ := range roots {
+		roots[i] = &Node{
+			0, 0, make([]int, 0), make(map[int]*Node), nil,
+		}
+	}
+	return &ForestLSH{
+		params, roots,
+	}
+}
+
+func NewForestLSHFromData(params *Params, dataset *Dataset) *ForestLSH {
+	forest := NewForestLSH(params)
+	for idx, point := range dataset.Points {
+		hvs := forest.Hash(point)
+		for i := range forest.Roots {
+			hv := hvs[i]
+			forest.Roots[i].insert(0, idx, &hv)
+		}
+	}
+	return forest
+}
--- forest_lsh_test.go
+++ forest_lsh_test.go
@@ -3,41 +3,13 @@ package golsh
 import (
 	"math/rand"
 	"testing"
-	// "fmt"
+	"fmt"
 )
 
-const (
-	TEST_INSTANCES = 10
-	TEST_RESULTS = 100
-	ROWS = 10000
-	VECLEN = 120
-	TABLESIZE = 10
-	KEYSIZE = 10
-	SLOT = 10.0
-)
-
-func Test_NewSimpleLSHFromData(t *testing.T) {
+func Test_Query_FOREST(t *testing.T) {
 	data := NewRandomData(ROWS, VECLEN)
 	params := NewParams(VECLEN, TABLESIZE, KEYSIZE, SLOT)
-	simpleLsh:= NewSimpleLSHFromData(params, data)
-
-	for _, table := range simpleLsh.Tables {
-		if len(table) == 0 {
-			t.Error("new simple lsh from data failed!")
-		}
-	}
-}
-
-func Test_Query(t *testing.T) {
-	data := NewRandomData(ROWS, VECLEN)
-	params := NewParams(VECLEN, TABLESIZE, KEYSIZE, SLOT)
-	simpleLsh:= NewSimpleLSHFromData(params, data)
-
-	for _, table := range simpleLsh.Tables {
-		if len(table) == 0 {
-			t.Error("new simple lsh from data failed!")
-		}
-	}
+	forestLsh := NewForestLSHFromData(params, data)
 
 	idx := rand.Perm(ROWS)[:TEST_INSTANCES]
 	validIdx := rand.Perm(ROWS)[:TEST_RESULTS]
@@ -48,7 +20,7 @@ func Test_Query(t *testing.T) {
 		p := data.Points[id]
 		avgDist := 0.0
 		count := 0
-		simpleLsh.Query(p, TEST_RESULTS, out)
+		forestLsh.Query(p, TEST_RESULTS, out)
 		for {
 			key := <- out
 			if key == -1 {
@@ -70,7 +42,7 @@ func Test_Query(t *testing.T) {
 		if avgDist > validAvgDist {
 			t.Error("neighbbours are further than random")
 		} else {
-			// fmt.Printf("num found %d, avg dist %f, valid dist %f\n", count, avgDist, validAvgDist)
+			fmt.Printf("num found %d, avg dist %f, valid dist %f\n", count, avgDist, validAvgDist)
 		}
 	}
 }
